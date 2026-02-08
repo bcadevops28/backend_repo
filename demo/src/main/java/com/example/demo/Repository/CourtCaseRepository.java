918fc05ab10e980acae137bb796310971f2a4902
vetrii-vs <vetrivs111@gmail.com>
2026-02-08 08:14:42 +0530

    Fix malformed pom.xml

--- demo/src/main/java/com/example/demo/Repository/CourtCaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.CourtCase;
-
-public interface CourtCaseRepository extends JpaRepository<CourtCase, Long> {
-}
--- src/main/java/com/example/demo/model/CaseStatus.java
+++ src/main/java/com/example/demo/model/CaseStatus.java
@@ -1,6 +1,4 @@
 package com.example.demo.model;
 
-public enum CaseStatus {
-    OPEN,
-    CLOSED
+public class CaseStatus {
 }
