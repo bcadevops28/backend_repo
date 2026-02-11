092ba7fbb38e27ba33c3f095014f88421c51ca68
VETRI V S <vetrivs111@gmail.com>
2026-02-11 13:12:01 +0530

    Update CourtCaseController.java

--- src/main/java/com/example/demo/Controller/CourtCaseController.java
+++ src/main/java/com/example/demo/Controller/CourtCaseController.java
@@ -1,6 +1,6 @@
 package com.example.demo.Controller;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,7 @@ public class CourtCaseController {
     @Autowired
     private CourtCaseService service;
 
-=======
+
 import com.example.demo.Entity.CourtCase;
 import com.example.demo.Entity.CaseType;
 import com.example.demo.Service.CourtCaseService;
@@ -41,13 +41,11 @@ public class CourtCaseController {
         return service.saveCase(courtCase);
     }
 
->>>>>>> 3c05b6fa735cae5be9f32e8988f3152240e79c2b
     @GetMapping
     public List<CourtCase> getAllCases() {
         return service.getAllCases();
     }
 
-<<<<<<< HEAD
     @PostMapping
     public CourtCase createCase(@RequestBody CourtCase courtCase) {
         return service.createCase(courtCase);
@@ -58,7 +56,6 @@ public class CourtCaseController {
             @PathVariable Long id,
             @RequestBody CourtCase updatedCase) {
         return service.updateCase(id, updatedCase);
-=======
     @GetMapping("/type/{type}")
     public List<CourtCase> getCasesByType(@PathVariable CaseType type) {
         return service.getCasesByType(type);
@@ -68,15 +65,11 @@ public class CourtCaseController {
     public CourtCase updateCase(@PathVariable Long id, @RequestBody CourtCase updatedCase) {
         updatedCase.setId(id);
         return service.saveCase(updatedCase);
->>>>>>> 3c05b6fa735cae5be9f32e8988f3152240e79c2b
     }
 
     @DeleteMapping("/{id}")
     public void deleteCase(@PathVariable Long id) {
         service.deleteCase(id);
     }
-<<<<<<< HEAD
 }
-=======
 }
->>>>>>> 3c05b6fa735cae5be9f32e8988f3152240e79c2b
